9c42eae8d9f66d634b1d02733d0f40dce936bfbe
Nicolas Castro Patiño <nicolascastropatino@gmail.com>
2025-02-25 22:50:01 +0100

    task add

---
+++ PruebaspringApplication.java
@@ -0,0 +1,20 @@
+package com.app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.app")
+@EnableJpaRepositories(basePackages = "com.app.repository")
+@EntityScan("com.app.entities")
+
+public class PruebaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaspringApplication.class, args);
+	}
+
+}
